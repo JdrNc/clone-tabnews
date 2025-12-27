cd7199df34388e9d6c416a081aaed0a18879c55f
Jordan Corrêa <101840416+JdrNc@users.noreply.github.com>
2025-12-27 15:23:19 +0000

    adiciona scripts `link:check` e `link:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone do domínio https://tabnews.com.br para aprimorar meus conhecimentos em programação
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
 function Home() {
-    return <h1>Você é o amor da minha e quero você ao meu lado sempreeee!!</h1>
+  return <h1>Você é o amor da minha e quero você ao meu lado sempreeee!!</h1>;
+}
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
 }
 export default Home;
