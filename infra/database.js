f5e22d18e0fb7907fd007f0dbf727c34ebf2332c
Jordan Corrêa <101840416+JdrNc@users.noreply.github.com>
2026-01-18 18:18:00 +0000

    `infra/database.js` more robust against errors

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,23 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-  });
+  const client = new Client(connectionObj);
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+  } finally {
+    await client.end();
+  }
 }
+const connectionObj = {
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+};
 export default {
   query: query,
 };
