c39204a35e948254d918a3116eae0342ae79cf34
Jordan Corrêa <101840416+JdrNc@users.noreply.github.com>
2026-01-31 15:15:05 +0000

    Make tests more robust with  `async-rety` and `orchestrator.js`
    	new file:   tests/orchestrator.js

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -1,5 +1,8 @@
 import database from "infra/database.js";
-
+import orchestrator from "tests/orchestrator.js";
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
 beforeAll(cleanDataBase);
 
 async function cleanDataBase() {
