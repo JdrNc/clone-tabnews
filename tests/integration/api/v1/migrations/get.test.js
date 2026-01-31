7ae279107a9fa6ba3c265009394b2fb2b259b83e
Jordan Corrêa <101840416+JdrNc@users.noreply.github.com>
2026-01-31 15:15:05 +0000

    Make tests more robust with  `async-rety` and `orchestrator.js`
    	modified:   jest.config.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   tests/integration/api/v1/migrations/delete.test.js
    	modified:   tests/integration/api/v1/migrations/get.test.js
    	modified:   tests/integration/api/v1/migrations/post.test.js
    	modified:   tests/integration/api/v1/status/get.test.js
    	new file:   tests/orchestrator.js

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,4 +1,8 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
 
 beforeAll(cleanDataBase);
 
